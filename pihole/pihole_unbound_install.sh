9dccc37ff90606162508ffc74646f0dc0388cc1a
wrosko <wrosko@drevidence.com>
2022-01-10 16:07:58 -0700

    pihole setup script

---
+++ pihole/pihole_unbound_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install tmux htop
+
+curl -sSL https://install.pi-hole.net | bash
+
+sudo apt install unbound
+
+mv pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
+
+sudo service unbound restart
+dig pi-hole.net @127.0.0.1 -p 5335
+
+echo "Change pihole IPv4 DNS settings to : 127.0.0.1#5335 \n https://docs.pi-hole.net/guides/dns/unbound/"
+echo "Done"
+
+
