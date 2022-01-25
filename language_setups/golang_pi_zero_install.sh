a5945095311b43cb3c9cdf66c8097d84306eed5d
wrosko <wrosko@drevidence.com>
2022-01-25 15:18:43 -0500

    docker-ify pihole

--- language_setups/golang_pi_zero_install.sh
+++ language_setups/golang_pi_zero_install.sh
@@ -4,6 +4,9 @@
 # Pi zero w uses armv6l architecture
 export GOLANG="$(curl https://go.dev/dl/|grep linux-armv6l|grep -v beta|head -1|awk -F\> {'print $3'}|awk -F\< {'print $1'})"
 wget https://golang.org/dl/$GOLANG
+echo "unzipping $GOLANG"
 sudo tar -C /usr/local -xzf $GOLANG
 rm $GOLANG
 unset GOLANG
+
+#https://www.e-tinkers.com/2019/06/better-way-to-install-golang-go-on-raspberry-pi/
